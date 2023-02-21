b829729180070da221bb40866f9e63a09f7cb844
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-02-21 19:20:05 +0200

    Task 8 complited

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,9 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 nameInput.addEventListener("input", () => {
-	nameOutput.textContent = nameInput.value;
-	if (nameInput.value === "") {
+	nameOutput.textContent = nameInput.value.trim();
+
+	if (nameOutput.textContent === "") {
 		nameOutput.textContent = "Anonymous";
 	}
 });
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+function checkFilling(event) {
+	event.preventDefault();
+	const { email, password } = event.currentTarget.elements;
+	
+	if (password.value === "" || email.value === "") {
+		alert("Поле Email і Password мають бути заповненні");
+		return;
+	}
+	const newUser = {
+		email: email.value,
+		password: password.value,
+	};
+	console.log("Данні користувача", newUser);
+	event.currentTarget.reset();
+}
+
+form.addEventListener("submit", checkFilling);
